7c0ac272e2cc63fc10012d2c53efb593f3d1dd13
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-09 13:00:35 +0300

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+        Employee firstEmployee = new Operator(
+                LocalDate.of(2022, 4, 9),
+                "Kirill",
+                40_000
+        );
     }
 }
