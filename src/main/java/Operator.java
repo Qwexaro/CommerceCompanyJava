bc83a9f0f84daa318ae4b34e2cf34ce23257debe
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-09 12:57:55 +0300

    main

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -1,3 +1,13 @@
-public class Operator {
+import java.time.LocalDate;
 
+public class Operator extends Employee {
+
+    public Operator(LocalDate dateStartWork, String name, double fixedSalary) {
+        super(dateStartWork, name, fixedSalary);
+    }
+
+    @Override
+    public double getMonthSalary() {
+        return getFixedSalary();
+    }
 }
