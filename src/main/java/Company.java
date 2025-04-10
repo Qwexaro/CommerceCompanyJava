556e8bb8660f00bc43c541454d0f8bfcd99b28ea
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-10 12:07:26 +0300

    main

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,3 +1,19 @@
-public class Company {
+import java.util.ArrayList;
+import java.util.Collection;
 
+public class Company {
+    private double incomeCompany;
+
+    private Collection<Employee> employees;
+    private ArrayList<Double> salariesEmployees;
+
+    public Company() {
+        incomeCompany = 0;
+        employees = new ArrayList<>();
+        salariesEmployees = new ArrayList<>();
+    }
+
+    public double getIncomeCompany() {
+        return incomeCompany;
+    }
 }
